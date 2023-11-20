1260384f551a378681f94249032102092a0c5b32
F_Dzserold <f.dzserold@gmail.com>
2023-11-20 14:37:59 +0100

    create a middleware and a function to handle possible errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,10 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   // Hash the password so it will be safe
   const hashedPassword = bcryptjs.hashSync(password, 10); //hashSync uses await so you dont need await for it
-
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     // Saves the date to mongoDB
@@ -14,6 +13,6 @@ export const signup = async (req, res) => {
     res.status(201).json("User created successfully");
   } catch (error) {
     // Response msg to client on error
-    res.status(500).json(error.message);
+    next(error);
   }
 };
